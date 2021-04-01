33a342a978a8dad9be12c887de3054f9d347ad13
Tishakin Nikolay <nikolay.tishakin@koroka.solutions>
2021-04-01 19:11:21 +0300

    Changed city type in register request dto

--- src/api-modules/user/user-modules/auth/models/dto/request/request-register.dto.ts
+++ src/api-modules/user/user-modules/auth/models/dto/request/request-register.dto.ts
@@ -1,12 +1,13 @@
-import {CreateImageDto} from '../../../../../../image/models/create-image.dto';
-import {GenderEnum} from '../../../../../enums/gender.enum';
+import { UserCityDto } from 'src/api-modules/user/models/dto/user-city.dto';
+import { CreateImageDto } from '../../../../../../image/models/create-image.dto';
+import { GenderEnum } from '../../../../../enums/gender.enum';
 
 export class RequestRegisterDto {
     firstName: string;
     lastName: string;
     nickname: string;
     phone: string;
-    city: string;
+    city: UserCityDto;
     birthday: Date;
     gender: GenderEnum;
     descriptions: string;
