4eee1ffb1a76b72d64a6d609878fb8fe359ce039
vlad.vyshnevskiy <vladyslav.vyshnevskyi@koroka.solutions>
2021-04-05 22:39:51 +0300

    edit update user

--- src/api-modules/user/user.repository.ts
+++ src/api-modules/user/user.repository.ts
@@ -3,6 +3,7 @@ import {User} from './models/user.entity';
 import {UpdateUserDto} from './models/dto/request/update-user.dto';
 import {UserFindError} from './errors/user-find.error';
 import {UserErrorEnum} from './enums/user-error.enum';
+import {UserAlreadyExistsError} from "./errors/user-already-exists.error";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -25,13 +26,21 @@ export class UserRepository extends Repository<User> {
     async updateUser(newUser: UpdateUserDto, id: number): Promise<User> {
         const user = await this.findOne(id, {relations: ['profileImage', 'city', 'city.country']});
 
-        if (user) {
+        if (!user) {
            throw new UserFindError([
                {
                    type: UserErrorEnum.NOT_FOUND,
                    details: 'Not found user by id: ' + id
                }
            ]);
+        }
+
+        if (await this.findOne(
+            {where: [
+                    {nickname: newUser.nickname}
+                ]},
+            )) {
+            throw new UserAlreadyExistsError();
         }
 
         const updateUser = Object.assign(user, newUser);
